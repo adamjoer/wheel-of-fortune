e6a17a3dda3f537ad0a8861ee1c69e3b12e3140b
Adam Harald Jørgensen <adamharald440@gmail.com>
2021-11-28 02:16:05 +0100

    Added view binding to the GameFragment class

--- app/src/main/java/dtu/ux62550/wheeloffortune/ui/game/GameFragment.kt
+++ app/src/main/java/dtu/ux62550/wheeloffortune/ui/game/GameFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import dtu.ux62550.wheeloffortune.R
+import dtu.ux62550.wheeloffortune.databinding.FragmentGameBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +21,8 @@ class GameFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var _binding: FragmentGameBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -32,9 +34,12 @@ class GameFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+
+        _binding = FragmentGameBinding.inflate(inflater)
+
+        return _binding.root
     }
 
     companion object {
