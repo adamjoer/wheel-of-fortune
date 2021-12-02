436fa46495bf55d37d90109c179fbdb103f3f3f6
Adam Harald Jørgensen <adamharald440@gmail.com>
2021-12-02 02:11:48 +0100

    Disabled landscape mode, so the layout doesn't get fucked

--- app/src/main/java/dtu/ux62550/wheeloffortune/GameConstants.kt
+++ app/src/main/java/dtu/ux62550/wheeloffortune/GameConstants.kt
@@ -90,3 +90,42 @@ val wordsAndCategories: List<Pair<String, String>> =
         Pair("Occupation", "Movie Critic"),
         Pair("Occupation", "Investigative Journalist")
     )
+
+enum class WheelResultType {
+    UNASSIGNED,
+    POINTS,
+    EXTRA_TURN,
+    LOSE_A_TURN,
+    BANKRUPT,
+}
+
+val wheelResults: List<Pair<WheelResultType, Int?>> =
+    listOf(
+        Pair(WheelResultType.POINTS, 100),
+        Pair(WheelResultType.POINTS, 200),
+        Pair(WheelResultType.POINTS, 300),
+        Pair(WheelResultType.POINTS, 400),
+        Pair(WheelResultType.POINTS, 500),
+        Pair(WheelResultType.POINTS, 550),
+        Pair(WheelResultType.POINTS, 600),
+        Pair(WheelResultType.POINTS, 600),
+        Pair(WheelResultType.POINTS, 600),
+        Pair(WheelResultType.POINTS, 650),
+        Pair(WheelResultType.POINTS, 650),
+        Pair(WheelResultType.POINTS, 700),
+        Pair(WheelResultType.POINTS, 700),
+        Pair(WheelResultType.POINTS, 750),
+        Pair(WheelResultType.POINTS, 800),
+        Pair(WheelResultType.POINTS, 900),
+        Pair(WheelResultType.POINTS, 950),
+        Pair(WheelResultType.POINTS, 1000),
+        Pair(WheelResultType.POINTS, 3500),
+
+        Pair(WheelResultType.LOSE_A_TURN, null),
+
+        Pair(WheelResultType.EXTRA_TURN, null),
+
+        Pair(WheelResultType.BANKRUPT, null),
+        Pair(WheelResultType.BANKRUPT, null),
+        Pair(WheelResultType.BANKRUPT, null),
+    )
